86e549e79d7b73861bca79ffdecc68a091850f32
miguel javela <mjavela@compuamerica.com.ve>
2022-04-10 21:09:29 -0500

    clase 10 - ¿que es el hoisting?

---
+++ src/hoisting/index.js
@@ -0,0 +1,12 @@
+a=2;
+var a;
+console.log(a);
+
+//javascript usa el hoisting en declaraciones y no en las inicializaciones
+console.log(b)
+var b = 2;
+
+nameOfDog("bruno");
+function nameOfDog(name){
+    console.log(name);
+}
