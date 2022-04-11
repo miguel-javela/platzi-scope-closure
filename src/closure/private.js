e07f35557e0e86cf0a15beec5282d1ee4f8ff3dc
miguel javela <mjavela@compuamerica.com.ve>
2022-04-10 20:55:48 -0500

    clase 8 - como crear variables privadas

---
+++ src/closure/private.js
@@ -0,0 +1,15 @@
+const person = () => {
+    saveName = "miguel";
+    return {
+        getName: () => {
+            return saveName;
+        },
+        setName: (name) => {
+            saveName = name;
+        }
+    }
+};
+newPerson = person();
+console.log(newPerson.getName());
+newPerson.setName('Oscar');
+console.log(newPerson.getName());
